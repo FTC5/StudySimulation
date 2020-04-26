05a27c7414748cc59287ed42c6bbafa6c2a173b4
FTC5 <dan.m1512@gmail.com>
2020-04-26 13:09:40 +0300

    StudySimulation :  StudySimulation.BLL refactor. Bug fix - edit EventManeger

--- StudySimulation.BLL/EventManager.cs
+++ StudySimulation.BLL/EventManager.cs
@@ -12,11 +12,16 @@ namespace StudySimulation.BLL
         Dictionary<string,List<IObserver>> eventObserver=new Dictionary<string, List<IObserver>>();
         GroupRating groupRating;
         List<Subject> subjects;
+        string[] key= { "getPoints", "message", "successFactor" };
 
         public EventManager(GroupRating groupRating, List<Subject> subjects)
         {
             this.groupRating = groupRating;
             this.subjects = subjects;
+            for (int i = 0; i < key.Length; i++)
+            {
+                eventObserver.Add(key[i], new List<IObserver>());
+            }
         }
         public EventManager(IUniversity university)
         {
@@ -38,52 +43,46 @@ namespace StudySimulation.BLL
         }
         public bool SubscribeToGetPoints(IObserver observer)
         {
-            string key = "getPoints";
             groupRating.GetEvaluation += (source, arg) => observer.Update(arg.text);
-            return Subscribe(key,observer);
+            return Subscribe(key[0],observer);//key[0]->getPoints
         }
         public void UnSubscribeToGetPoints(IObserver observer)
         {
 
-            string key = "getPoints";
             groupRating.GetEvaluation -= (source, arg) => observer.Update(arg.text);
-            UnSubscribe(key, observer);
+            UnSubscribe(key[0], observer);//key[0]->getPoints
         }
         public bool SubscribeToMessage(IObserver observer)
         {
-            string key = "message";
             foreach (Subject item in subjects)
             {
                 item.Message+= (source, arg) => observer.Update(arg.text);
             }
-            return Subscribe(key, observer);
+            return Subscribe(key[1], observer);//key[1]->message
         }
         public void UnSubscribeToMessage(IObserver observer)
         {
-            string key = "message";
             foreach (Subject item in subjects)
             {
                 item.Message -= (source, arg) => observer.Update(arg.text);
             }
-            UnSubscribe(key, observer);
+            UnSubscribe(key[1], observer);//key[1]->message
         }
         public bool SubscribeToSuccessFactor(IObserver observer)
         {
-            string key = "successFactor";
             foreach (Subject item in subjects)
             {
                 item.Factor += (source, arg) => observer.Update(arg.text,arg.successFactor);
             }
-            return Subscribe(key, observer);
+            return Subscribe(key[2], observer);//key[2]->successFactor
         }
         public void UnSubscribeToSuccessFactor(IObserver observer)
         {
-            string key = "successFactor";
             foreach (Subject item in subjects)
             {
                 item.Factor -= (source, arg) => observer.Update(arg.text, arg.successFactor);
             }
-            UnSubscribe(key, observer);
+            UnSubscribe(key[2], observer);//key[2]->successFactor
         }
     }
 }
